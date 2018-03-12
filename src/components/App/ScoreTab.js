582c87fb935d05d87df24ed88fbe90720c6e4001
Steve Occhipinti <dev@stevenocchipinti.com>
2018-03-12 22:58:04 +1100

    A basic desktop layout that looks ok
    This is the first part of issue #7, desktop looks ok but there is probably room
    for improvement.

--- src/components/App/ScoreTab.js
+++ src/components/App/ScoreTab.js
@@ -3,10 +3,19 @@ import TabPanel from './TabPanel'
 import CurrentPlayerPanel from '../CurrentPlayerPanel'
 import OtherPlayersPanel from '../OtherPlayersPanel'
 import ActionPanel from '../ActionPanel'
+import {largeBreakpointWidth} from '../../config-constants'
 
 const TabPanelContainer = TabPanel.extend`
   display: flex;
   flex-direction: column;
+
+  @media (min-width: ${largeBreakpointWidth}px) {
+    display: grid;
+    grid-template-columns: 5fr 1fr;
+    grid-template-areas:
+      'current-player action-panel'
+      'other-players  action-panel';
+  }
 `
 
 export default ({players, currentPlayerIndex}) => {
--- src/components/App/TabPanel.js
+++ src/components/App/TabPanel.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export default styled.main`
   position: relative;
+  height: 100%;
   flex-grow: 1;
   overflow: scroll;
 `
